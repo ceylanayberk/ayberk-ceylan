fd382801c572983595ac831a102fe6c06e3a62b6
ceylanayberk <57677000+ceylanayberk@users.noreply.github.com>
2020-09-30 13:34:44 +0300

    phpv1

--- index.php
+++ index.php
@@ -10,10 +10,14 @@
         margin: 0;
         padding: 0;
         text-align: center;
+        max-width: 600;
     }
 </style>
 </head>
 <body>
 <h1>AYBERK CEYLAN</h1>
+<div>
+<p style="text-decoration: none; font-colour: red;"><a href="">Instagram</a><a href="">Twitter</a></p>
+</div>
 </body>
 </html>
